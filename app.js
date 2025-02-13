e0d09ad540e999bdf2fbab3f18c4bca3786a275d
葉子_葉子慧 <angel1996421@gmail.com>
2025-02-13 22:02:20 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 function App() {
   return (
     <div className="container mx-auto p-4">
-      <div class="row">
-        <div class="col-md-4">
-          <div class="bg-light p-3">
+      <div className="row">
+        <div className="col-md-4">
+          <div className="bg-light p-3">
             <img
               src="https://randomuser.me/api/portraits/women/61.jpg"
               alt="頭像"
-              class="img-fluid rounded-circle"
+              className="img-fluid rounded-circle"
             />
-            <h2 class="mb-0">Mona Heen</h2>
-            <p class="mb-0">mona.heen@example.com</p>
+            <h2 className="mb-0">Mona Heen</h2>
+            <p className="mb-0">mona.heen@example.com</p>
           </div>
         </div>
-        <div class="col-md-4">
-          <div class="bg-light p-3">
+        <div className="col-md-4">
+          <div className="bg-light p-3">
             <img
               src="https://randomuser.me/api/portraits/women/10.jpg"
               alt="頭像"
-              class="img-fluid rounded-circle"
+              className="img-fluid rounded-circle"
             />
-            <h2 class="mb-0">Susan Craig</h2>
-            <p class="mb-0">susan.craig@example.com</p>
+            <h2 className="mb-0">Susan Craig</h2>
+            <p className="mb-0">susan.craig@example.com</p>
           </div>
         </div>
       </div>
